3f0acca332d20bfa0ece6a51bdc31c9b9b63b7a9
huydcg <huydcg.21it@vku.udn.vn>
2025-10-18 09:49:36 +0700

    update

---
+++ SpaceExplorer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Nhập phím mũi tên
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        moveInput = new Vector2(moveX, moveY).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = moveInput * moveSpeed;
+    }
+}
